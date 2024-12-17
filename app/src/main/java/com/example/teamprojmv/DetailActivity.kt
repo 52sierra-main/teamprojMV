d6ba7ed7978e08fc08ce75b5c472fb70c3a3a4e6
52sierra-main <sohojang320@gmail.com>
2024-12-17 20:08:53 +0900

    beta code

---
+++ app/src/main/java/com/example/teamprojmv/DetailActivity.kt
@@ -0,0 +1,47 @@
+package com.example.teamprojmv
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.net.toUri
+import com.bumptech.glide.Glide
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        // Back
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        // detail info?
+        val title = intent.getStringExtra("title") ?: "Unknown Title"
+        val imageUrl = intent.getStringExtra("imageUrl") ?: ""
+
+        // Reference Views
+        val titleView: TextView = findViewById(R.id.movieTitle)
+        val overview: TextView = findViewById(R.id.movieOverview)
+        val poster: ImageView = findViewById(R.id.moviePoster)
+
+        // Set Movie Details
+        titleView.text = title
+        overview.text = "임시 $title."
+        Glide.with(this).load(imageUrl.toUri()).into(poster)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish() // Close the current activity
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
